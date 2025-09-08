6fa242da62501e09fb699f18e11b06cd6568bd92
Hamza Abu Ayyash <hamzaabuayyash@Hamzas-MacBook-Pro.local>
2025-09-08 17:34:54 +0300

    refactor: Use absolute package imports for maximum reliability

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # app.py
+import os
+import sys
 
 # --- THE GOLDEN FIX: Force the project root into the Python path ---
 # This explicitly tells the running script where to find its packages.
--- worker.py
+++ worker.py
@@ -1,4 +1,6 @@
 # worker.py
+import os
+import sys
 
 # --- THE GOLDEN FIX: Force the project root into the Python path ---
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
